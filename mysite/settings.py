406712ef2d0e55a8cdd19845bbd5ea8e25af2fc3
Brian Paur <brian.paur@gmail.com>
2025-07-08 23:46:37 -0500

    DEBUG = False

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-key")
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
 DEBUG = True
 # ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "localhost")]
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['beerdashboardproject.onrender.com','127.0.0.1']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['beerdashboardproject.onrender.com','127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://beerdashboardproject.onrender.com",
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
