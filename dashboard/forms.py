9f2ace19f64b073da45dc846e1919e1818ab4791
Brian Paur <brian.paur@gmail.com>
2025-12-06 16:28:13 -0600

    added metric data

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from datetime import date, timedelta
 from tuya_connector import TuyaOpenAPI
-from dashboard.creds.creds import ACCESS_ID, ACCESS_KEY, ENDPOINT, DEVICE_ID,DEVICE_ID2
-# import json
-# with open('/etc/secrets/creds.json') as f:
-#     creds = json.load(f)
-#
-# ACCESS_ID = creds['ACCESS_ID']
-# ACCESS_KEY = creds['ACCESS_KEY']
-# ENDPOINT = creds['ENDPOINT']
-# DEVICE_ID = creds['DEVICE_ID']
-# DEVICE_ID2 = creds['DEVICE_ID2']
+# from dashboard.creds.creds import ACCESS_ID, ACCESS_KEY, ENDPOINT, DEVICE_ID,DEVICE_ID2
+import json
+with open('/etc/secrets/creds.json') as f:
+    creds = json.load(f)
+
+ACCESS_ID = creds['ACCESS_ID']
+ACCESS_KEY = creds['ACCESS_KEY']
+ENDPOINT = creds['ENDPOINT']
+DEVICE_ID = creds['DEVICE_ID']
+DEVICE_ID2 = creds['DEVICE_ID2']
 
 from django import forms
 from .models import GoogleSheetSourceData, FermentationDataTilt
