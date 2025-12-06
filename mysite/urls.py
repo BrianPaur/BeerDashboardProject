f6befc509bacf07f81dbb3c27e870f8aaadce0ad
Brian Paur <brian.paur@gmail.com>
2025-12-06 16:23:47 -0600

    added metric data

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("dashboard.urls")),
     path('admin/', admin.site.urls),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
