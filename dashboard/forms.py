c2e69f9c749d7ca675aeb091794bd8f0eea41155
Brian Paur <brian.paur@gmail.com>
2026-01-25 17:55:00 -0600

    Accidentally uploaded forms.py with incorrect creds...

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from datetime import date, timedelta
 from tuya_connector import TuyaOpenAPI
 # from dashboard.creds.creds import ACCESS_ID, ACCESS_KEY, ENDPOINT, DEVICE_ID,DEVICE_ID2
-# import json
-# with open('/etc/secrets/creds.json') as f:
-#     creds = json.load(f)
-#
+import json
+with open('/etc/secrets/creds.json') as f:
+    creds = json.load(f)
+
 ACCESS_ID = creds['ACCESS_ID']
 ACCESS_KEY = creds['ACCESS_KEY']
 ENDPOINT = creds['ENDPOINT']
