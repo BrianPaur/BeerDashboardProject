625bcd6f08602061c14068e30ad30fcf5bcb8100
Brian Paur <brian.paur@gmail.com>
2026-09-18 22:55:38 -0500

    Phase 4: Updated inkbird.py creds

--- dashboard/services/inkbird.py
+++ dashboard/services/inkbird.py
@@ -1,12 +1,14 @@
+import json
 from tuya_connector import TuyaOpenAPI
 
-from dashboard.creds.creds import (
-    ACCESS_ID,
-    ACCESS_KEY,
-    ENDPOINT,
-    DEVICE_ID,
-    DEVICE_ID2,
-)
+with open('/etc/secrets/creds.json') as f:
+    creds = json.load(f)
+
+ACCESS_ID = creds['ACCESS_ID']
+ACCESS_KEY = creds['ACCESS_KEY']
+ENDPOINT = creds['ENDPOINT']
+DEVICE_ID = creds['DEVICE_ID']
+DEVICE_ID2 = creds['DEVICE_ID2']
 
 
 class InkbirdService:
